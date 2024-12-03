56f6673a7c0e9fec97f4506d410e13cc5f2f5ad7
rafibhatia <159935496+rafibhatia@users.noreply.github.com>
2024-12-03 16:28:58 -0500

    step two

---
+++ fun.py
@@ -0,0 +1,9 @@
+def tziv(first, last):
+    print(f'hi there {first} and {last}')
+
+
+rafi = tziv('rafi', 'bhatia')
+
+
+
+name = input('enter your name')
--- tic_tok_tow.py
+++ tic_tok_tow.py
@@ -7,6 +7,7 @@ def display_board(board):
     print(board[3] + ' | ' + board[4] + ' | ' + board[5])
     print('---------')
     print(board[6] + ' | ' + board[7] + ' | ' + board[8])
+    
 
 
 def player_move(board, player):
